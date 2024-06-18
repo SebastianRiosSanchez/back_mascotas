cd271b1f24c021edc98ababb830f3e76c6119af1
SebastianRiosSanchez <sebastianrios47@gmail.com>
2024-06-18 00:11:02 -0500

    avances imagen

--- src/main/java/com/petshop/tienda/controllers/MascotaController.java
+++ src/main/java/com/petshop/tienda/controllers/MascotaController.java
@@ -2,14 +2,22 @@ package com.petshop.tienda.controllers;
 
 import com.petshop.tienda.models.Mascota;
 import com.petshop.tienda.services.MascotaService;
+import jakarta.annotation.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Optional;
 
@@ -25,6 +33,47 @@ public class MascotaController {
     @PostMapping
     public Mascota crearMascota(@RequestBody Mascota nuevaMascota) {
         return mascotaService.crearMascota(nuevaMascota);
+    }
+
+    //UploadFile
+    @PostMapping("/upload")
+    public String uploadFile(@RequestParam("file") MultipartFile file) {
+        if (file.isEmpty()) {
+            return "Sin archivo para guardar";
+        }
+        try {
+// Get the file and save it somewhere
+            byte[] bytes = file.getBytes();
+            // Define the path where the file will be saved
+            String pathSave = "C:/Users/sebas/OneDrive/Escritorio/Java/tienda/src/main/resources/images";
+//            Path path = Paths.get("images" + File.separator + file.getOriginalFilename());
+            Path path = Paths.get(pathSave + File.separator + file.getOriginalFilename());
+            Files.write(path, bytes);
+
+            return "You successfully uploaded '" + file.getOriginalFilename() + "'";
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "Failed to upload '" + file.getOriginalFilename() + "'";
+        }
+    }
+
+    // Get Uploaded File
+    @GetMapping("/image/{imageName}")
+    public ResponseEntity<Resource> getImage(@PathVariable String imageName) throws MalformedURLException {
+        final String UPLOAD_DIR = "C:/Users/sebas/OneDrive/Escritorio/Java/tienda/src/main/resources/images";
+
+        Path imagePath = Paths.get(UPLOAD_DIR).resolve(imageName);
+        org.springframework.core.io.Resource resource = new UrlResource(imagePath.toUri());
+
+        if (resource.exists() && resource.isReadable()) {
+            return ResponseEntity.ok()
+                    .contentType(MediaType.IMAGE_JPEG) // Ajusta el tipo MIME según el tipo de imagen que manejas
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=\"" + resource.getFilename() + "\"")
+                    .body((Resource) resource);
+        } else {
+            throw new RuntimeException("No se pudo cargar la imagen: " + imageName);
+        }
+
     }
 
     //Get
